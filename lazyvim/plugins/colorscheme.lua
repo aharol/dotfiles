4d3d925a5d958fb5c1cf8805c65f86906d6e29c1
Art <art.harol@flintworks.io>
2025-05-01 13:51:32 +1000

    updated structure

---
+++ lazyvim/plugins/colorscheme.lua
@@ -0,0 +1,43 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if (vim.g.colors_name or ""):find("catppuccin") then
+        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+      end
+    end,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        direction = "horizontal",
+        open_mapping = [[<c-\>]],
+        shade_terminals = false,
+        -- add --login so ~/.zprofile is loaded
+        -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+        shell = "zsh --login",
+      })
+    end,
+    keys = {
+      { [[<C-\>]] },
+      { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+      {
+        "<leader>td",
+        "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
+        desc = "Open a horizontal terminal at the Desktop directory",
+      },
+    },
+  },
+}
